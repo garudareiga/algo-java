f0db4130d59a5a6539f06bf7251df9dd22412f4a
garudareiga <garudareiga@gmail.com>
2014-11-14 17:29:55 -0800

    Add convert bst to doubly linked list

--- src/main/java/tree/ConvertBT2DoublyLinkedList.java
+++ src/main/java/tree/ConvertBT2DoublyLinkedList.java
@@ -1,7 +1,5 @@
 package tree;
 
-import java.util.Stack;
-
 import util.TreeNode;
 
 /**
@@ -13,23 +11,52 @@ import util.TreeNode;
  */
 
 public class ConvertBT2DoublyLinkedList {
+    TreeNode head = null, prev = null;
 
-    TreeNode convertIterative(TreeNode root) {
-        if (root == null) return null;
-        
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.push(root);
-        
-        TreeNode head = TreeNode.getMinTreeNode(root);
-        TreeNode prev = TreeNode.getMaxTreeNode(root);
-        while (!stack.isEmpty()) {
-            TreeNode n = stack.pop();
-            if (n.right != null) stack.push(n.right);
-            if (n.left != null) stack.push(n.left);
-            prev.right = n;
-            n.left = prev;
-            prev = n;
-        }
+    public TreeNode convert(TreeNode root) {
+        convertRecursive(root);
         return head;
+    }
+    
+    void convertRecursive(TreeNode root) {
+        if (root == null) return;
+        convertRecursive(root.left);
+        if (prev == null) head = root;
+        else {
+            prev.right = root;
+            root.left = prev;
+        }
+        prev = root;
+        convertRecursive(root.right);
+    }
+    
+    public static void main(String[] args) {
+        /*    binary tree
+         *           30                  
+         *          /  \                
+         *        20    40               
+         *       /     /  \                   
+         *     10     35  50                
+         *                
+         */                           
+         TreeNode node_1 = new TreeNode(30),
+                 node_2 = new TreeNode(20),
+                 node_3 = new TreeNode(40),
+                 node_4 = new TreeNode(10),
+                 node_5 = new TreeNode(35),
+                 node_6 = new TreeNode(50);
+         node_1.left = node_2;
+         node_1.right = node_3;
+         node_2.left = node_4;
+         node_3.left = node_5;
+         node_3.right = node_6;
+         
+         TreeNode root = node_1;
+         ConvertBT2DoublyLinkedList sol = new ConvertBT2DoublyLinkedList();
+         TreeNode head = sol.convert(root);
+         while (head != null) {
+             System.out.print(head.val + " ");
+             head = head.right;
+         }
     }
 }
